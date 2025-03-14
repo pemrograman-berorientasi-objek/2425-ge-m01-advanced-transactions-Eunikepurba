9633b76b906439f76ca3c55428c7b1dd510a1d30
Eunikepurba <eunikepurba50@gmail.com>
2025-03-14 15:22:03 +0700

    pair_02

--- src/fintech/driver/Driver1.java
+++ src/fintech/driver/Driver1.java
@@ -1,15 +1,34 @@
-package fintech.driver;
+package fintech.driver; 
+
+import java.util.Scanner; 
+import fintech.model.*;
 
 /**
- * @author NIM Nama
- * @author NIM Nama
+ * @autor 12S23041- Lisbeth Panjaitan
+ * @autor 12S23021- Eunike Purba
  */
+
 public class Driver1 {
 
     public static void main(String[] _args) {
 
-        // codes
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine();
+        String accountName = scanner.nextLine();
+        String accountOwner = scanner.nextLine();
 
+        Account newAccount = new Account(accountOwner, accountName);
+        System.out.println(newAccount);
+
+        command = scanner.nextLine();
+        String transactionAccount = scanner.nextLine();
+        double transactionAmount = Double.parseDouble(scanner.nextLine());
+        String transactionDate = scanner.nextLine();
+        String transactionNote = scanner.nextLine();
+
+        Transaction newTransaction = new Transaction(transactionAccount, transactionAmount, transactionDate, transactionNote);
+        System.out.println(newTransaction);
+
+        scanner.close();
     }
-
 }
